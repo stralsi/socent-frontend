00bed951e9fa3213ae3fc4dc53444a1cc2f81b99
Cezar Neaga <hi@cezarneaga.eu>
2016-10-20 10:58:21 +0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to Socent</h2>
+        </div>
+        <p className="App-intro">
+          Are you feeling pumped?
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
